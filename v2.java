8b2b53c0e82e08c6c298deb2e7362589c0c6f74a
Bengcord <65516257+Bengcord@users.noreply.github.com>
2021-02-17 07:32:21 +0800

    Create v2.java

---
+++ v2.java
@@ -0,0 +1,4 @@
+@echo off
+title Creative
+java -server -Xincgc -Xmx1024M -Xms128M -Xss512K -XX:+AggressiveOpts -XX:+UseCompressedOops -XX:+UseCMSCompactAtFullCollection -XX:+UseFastAccessorMethods -XX:ParallelGCThreads=4 -XX:+UseConcMarkSweepGC -XX:CMSFullGCsBeforeCompaction=2 -XX:CMSInitiatingOccupancyFraction=70 -XX:-DisableExplicitGC -XX:TargetSurvivorRatio=90 -jar paper-1.16.5-438.jar nogui
+pause
